b5d67b257ec49f9cb51a9f5f0481e155e0bdee4a
nullifiedcat <nullifiedcat@gmail.com>
2017-12-19 23:58:58 +0300

    update

--- src/logging.cpp
+++ src/logging.cpp
@@ -18,7 +18,7 @@ void logging::Initialize()
 {
     // FIXME other method of naming the file?
     passwd *pwd     = getpwuid(getuid());
-    logging::handle = fopen(strfmt("/tmp/cathook-%s.log", pwd->pw_name), "w");
+    logging::handle = fopen(strfmt("/tmp/cathook-%s-%d.log", pwd->pw_name, getpid()), "w");
 }
 
 void logging::Info(const char *fmt, ...)
