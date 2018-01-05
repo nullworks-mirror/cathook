cfe48345a5d1a517e8b52667ac39d1da04ffba1d
BenCat07 <BenCat07@gmail.com>
2018-01-05 16:17:49 +0100

    oof fix

--- src/hacks/CatBot.cpp
+++ src/hacks/CatBot.cpp
@@ -97,7 +97,7 @@ void update_catbot_list()
 
 		info.name[31] = 0;
 		if (strcasestr(info.name, "cat-bot") ||
-			strcasestr(info.name, "just disable vac tf")) || 
+			strcasestr(info.name, "just disable vac tf") || 
 			strcasestr(info.name, "raul.garcia") || 
 			strcasestr(info.name, "zCat"))
 		{
