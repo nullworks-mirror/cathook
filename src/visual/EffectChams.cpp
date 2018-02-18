9fc27056897c0371f810650c193dee378a873ad8
BenCat07 <BenCat07@gmail.com>
2018-02-18 10:13:08 +0100

    do not render buildings carried by lcoal player

--- src/visual/EffectChams.cpp
+++ src/visual/EffectChams.cpp
@@ -147,6 +147,10 @@ bool EffectChams::ShouldRenderChams(IClientEntity *entity)
             return false;
         if (!ent->m_bEnemy && !(teammate_buildings || teammates))
             return false;
+        if (ent->m_iHealth == 0 || !ent->m_iHealth)
+        	return false;
+        if (CE_BYTE(LOCAL_E, netvar.m_bCarryingObject) && LOCAL_E->m_vecOrigin.DistTo(ent->m_vecOrigin) <= 85.0f)
+        	return false;
         return true;
     case ENTITY_PLAYER:
         if (!players)
