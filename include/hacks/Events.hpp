143566bef747cb6e6153d1efaef89a4da7ba45eb
BenCat07 <BenCat07@gmail.com>
2018-02-25 13:28:42 +0100

    Events.hpp

---
+++ include/hacks/Events.hpp
@@ -0,0 +1,22 @@
+/*
+ * Events.hpp
+ *
+ *  Created on: Feb 24, 2018
+ *      Author: bencat07
+ */
+#pragma once
+
+#include "common.hpp"
+
+namespace hacks
+{
+namespace shared
+{
+namespace event
+{
+
+void CreateMove();
+char *inttochar(int i);
+}
+}
+}
