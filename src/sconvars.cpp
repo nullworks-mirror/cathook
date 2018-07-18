903f0c1c361d83f124c7cb18cb7c61c05d65930a
TotallyNotElite <1yourexperiment@protonmail.com>
2018-07-18 19:38:59 +0200

    unique_ptr tf

--- src/sconvars.cpp
+++ src/sconvars.cpp
@@ -16,7 +16,7 @@ SpoofedConVar::SpoofedConVar(ConVar *var) : original(var)
 {
     int flags        = var->m_nFlags;
     const char *name = var->m_pszName;
-    char *s_name     = strfmt("q_%s", name);
+    char *s_name     = strfmt("q_%s", name).get();
     if (g_ICvar->FindVar(s_name))
         return;
     var->m_pszName = s_name;
