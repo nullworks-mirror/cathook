f74b24710be64fbbca093f24cb12972f3270bd56
TotallyNotElite <1yourexperiment@protonmail.com>
2018-09-27 08:40:35 +0200

    Improve tm #584

--- src/votelogger.cpp
+++ src/votelogger.cpp
@@ -9,10 +9,9 @@
 #include <boost/algorithm/string.hpp>
 #include <settings/Bool.hpp>
 
-static settings::Bool requeue{ "hack.requeue-on-kick", "false" };
-static settings::Bool vote_kicky{ "hack.autovote.yes", "false" };
-static settings::Bool vote_kickn{ "hack.autovote.no", "false" };
-static settings::Bool party_say{ "hack.partysay", "true" };
+static settings::Bool vote_kicky{ "votelogger.autovote.yes", "false" };
+static settings::Bool vote_kickn{ "votelogger.autovote.no", "false" };
+static settings::Bool party_say{ "votelogger.partysay", "true" };
 
 namespace votelogger
 {
@@ -30,8 +29,8 @@ void dispatchUserMessage(bf_read &buffer, int type)
     {
         // TODO: Add always vote no/vote no on friends. Cvar is "vote option2"
         was_local_player = false;
-        int team	=	buffer.ReadByte();
-        int caller      =	buffer.ReadByte();	//	client id of init apparently
+        int team         = buffer.ReadByte();
+        int caller       = buffer.ReadByte(); //	client id of init apparently
         char reason[64];
         char name[64];
         buffer.ReadString(reason, 64, false, nullptr);
@@ -41,9 +40,12 @@ void dispatchUserMessage(bf_read &buffer, int type)
         eid >>= 1;
 
         unsigned steamID = 0;
-        player_info_s info{}, info2{};	//	info is the person getting kicked, info2 is the person calling the kick.;
-        if (g_IEngine->GetPlayerInfo(eid, &info))
-            steamID = info.friendsID;
+        // info is the person getting kicked,
+        // info2 is the person calling the kick.
+        player_info_s info{}, info2{};
+        if (!g_IEngine->GetPlayerInfo(eid, &info))
+            break;
+        steamID = info.friendsID;
         if (eid == LOCAL_E->m_IDX)
             was_local_player = true;
         if (*vote_kickn)
@@ -60,14 +62,21 @@ void dispatchUserMessage(bf_read &buffer, int type)
                 g_IEngine->ClientCmd_Unrestricted("vote option1");
         if (*party_say)
         {
-        	g_IEngine->GetPlayerInfo(caller, &info2);
-        	g_IEngine->ExecuteClientCmd(format("say_party [cat] votekick called: ",
-        			boost::replace_all_copy((std::string)info2.name, ";", ";"),
-					" => ",
-        			boost::replace_all_copy((std::string)info.name, ";", ";"),
-        			" (",reason,")").c_str());
-        	//	because tf2 is stupid and doesn't have escape characters, i'll use the greek question mark instead.
-        	//	big brain.
+            if (g_IEngine->GetPlayerInfo(caller, &info2))
+            {
+                // because tf2 is stupid and doesn't have escape characters,
+                // use the greek question marks instead. big brain.
+                // Clang-format why, TODO: Don't use format func
+                g_IEngine->ExecuteClientCmd(
+                    format("say_party [cat] votekick called: ",
+                           boost::replace_all_copy((std::string) info2.name,
+                                                   ";", ";"),
+                           " => ",
+                           boost::replace_all_copy((std::string) info.name, ";",
+                                                   ";"),
+                           " (", reason, ")")
+                        .c_str());
+            }
         }
         logging::Info("Vote called to kick %s [U:1:%u] for %s", name, steamID,
                       reason);
@@ -75,8 +84,8 @@ void dispatchUserMessage(bf_read &buffer, int type)
     }
     case 47:
         logging::Info("Vote passed");
-        if (was_local_player && requeue)
-            tfmm::startQueue();
+        //        if (was_local_player && requeue)
+        //            tfmm::startQueue();
         break;
     case 48:
         logging::Info("Vote failed");
