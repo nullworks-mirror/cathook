af6211418774adfd6ceb988aba2f1a3289995461
TotallyNotElite <1yourexperiment@protonmail.com>
2018-11-11 00:13:17 +0100

    New stacktrace crash handler
    thank you boost!

--- src/hack.cpp
+++ src/hack.cpp
@@ -5,22 +5,14 @@
  *      Author: nullifiedcat
  */
 
-// The code below was obtained from:
-// http://stackoverflow.com/questions/77005/how-to-generate-a-stacktrace-when-my-gcc-c-app-crashes/1925461#1925461
-#ifndef _GNU_SOURCE
-#define _GNU_SOURCE
-#endif
-#ifndef __USE_GNU
 #define __USE_GNU
-#endif
-
 #include <execinfo.h>
 #include <signal.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <ucontext.h>
-#include <unistd.h>
+#include <dlfcn.h>
+#include <boost/stacktrace.hpp>
+#include <cxxabi.h>
 #include <visual/SDLHooks.hpp>
 #include "hack.hpp"
 #include "common.hpp"
@@ -33,6 +25,7 @@
 
 #include "copypasted/CDumper.hpp"
 #include "version.h"
+#include <cxxabi.h>
 
 /*
  *  Credits to josh33901 aka F1ssi0N for butifel F1Public and Darkstorm 2015
@@ -105,85 +98,56 @@ void hack::ExecuteCommand(const std::string command)
     hack::command_stack().push(command);
 }
 
-/* This structure mirrors the one found in /usr/include/asm/ucontext.h */
-typedef struct _sig_ucontext
+void my_signal_handler(int signum)
 {
-    unsigned long uc_flags;
-    struct ucontext *uc_link;
-    stack_t uc_stack;
-    struct sigcontext uc_mcontext;
-    sigset_t uc_sigmask;
-} sig_ucontext_t;
+    namespace st = boost::stacktrace;
+    ::signal(signum, SIG_DFL);
+    passwd *pwd = getpwuid(getuid());
+    std::ofstream out(
+        strfmt("/tmp/cathook-%s-%d-segfault.log", pwd->pw_name, getpid())
+            .get());
 
-void crit_err_hdlr(int sig_num, siginfo_t *info, void *ucontext)
-{
-    void *array[50];
-    void *caller_address;
-    char **messages;
-    int size, i;
-    sig_ucontext_t *uc;
+    Dl_info info;
+    if (!dladdr(reinterpret_cast<void *>(SetCanshootStatus), &info))
+        return;
+    unsigned int baseaddr = (unsigned int) info.dli_fbase;
 
-    uc = (sig_ucontext_t *) ucontext;
-
-/* Get the address at the time the signal was raised */
-#if defined(__i386__)                              // gcc specific
-    caller_address = (void *) uc->uc_mcontext.eip; // EIP: x86 specific
-#elif defined(__x86_64__)                          // gcc specific
-    caller_address = (void *) uc->uc_mcontext.rip; // RIP: x86_64 specific
-#else
-#error Unsupported architecture. // TODO: Add support for other arch.
-#endif
-
-    fprintf(stderr, "\n");
-    FILE *backtraceFile;
-
-    // In this example we write the stacktrace to a file. However, we can also
-    // just fprintf to stderr (or do both).
-    passwd *pwd   = getpwuid(getuid());
-    backtraceFile = fopen(
-        strfmt("/tmp/cathook-%s-%d-segfault.log", pwd->pw_name, getpid()).get(),
-        "w");
-
-    if (sig_num == SIGSEGV)
-        fprintf(backtraceFile, "signal %d (%s), address is %p from %p\n",
-                sig_num, strsignal(sig_num), info->si_addr,
-                (void *) caller_address);
-    else
-        fprintf(backtraceFile, "signal %d (%s)\n", sig_num, strsignal(sig_num));
-
-    size = backtrace(array, 50);
-    /* overwrite sigaction with caller's address */
-    array[1] = caller_address;
-    messages = backtrace_symbols(array, size);
-    /* skip first stack frame (points here) */
-    for (i = 1; i < size && messages != NULL; ++i)
+    for (auto i : st::stacktrace())
     {
-        fprintf(backtraceFile, "[bt]: (%d) %s\n", i, messages[i]);
+        unsigned int offset = (unsigned int) (i.address()) - baseaddr;
+        Dl_info info2;
+        out << (void *)offset;
+        if (!dladdr(i.address(), &info2))
+        {
+            out << std::endl;
+            continue;
+        }
+        out << " " << info2.dli_fname << ": ";
+        if (info2.dli_sname)
+        {
+            int status = -4;
+            char *realname =
+                abi::__cxa_demangle(info2.dli_sname, 0, 0, &status);
+            if (status == 0)
+            {
+                out << realname << std::endl;
+                free(realname);
+            }
+            else
+                out << info2.dli_sname << std::endl;
+        }
+        else
+            out << "Not found!" << std ::endl;
     }
 
-    fclose(backtraceFile);
-    free(messages);
-
-    std::abort();
-    exit(EXIT_FAILURE);
+    out.close();
+    ::raise(SIGABRT);
 }
 
-void installSignal(int __sig)
-{
-    struct sigaction sigact;
-    sigact.sa_sigaction = crit_err_hdlr;
-    sigact.sa_flags     = SA_RESTART | SA_SIGINFO;
-    if (sigaction(__sig, &sigact, (struct sigaction *) NULL) != 0)
-    {
-        fprintf(stderr, "error setting signal handler for %d (%s)\n", __sig,
-                strsignal(__sig));
-        exit(EXIT_FAILURE);
-    }
-}
 void hack::Initialize()
 {
-    signal(SIGPIPE, SIG_IGN);
-    installSignal(SIGSEGV);
+    ::signal(SIGSEGV, &my_signal_handler);
+    //::signal(SIGABRT, &my_signal_handler);
     time_injected = time(nullptr);
 /*passwd *pwd   = getpwuid(getuid());
 char *logname = strfmt("/tmp/cathook-game-stdout-%s-%u.log", pwd->pw_name,
