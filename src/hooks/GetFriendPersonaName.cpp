70c724bf519b4ab16429dd0459c67a602cb5115d
Unnamed <user@localhost.localdomain>
2018-12-19 12:27:57 +0000

    Improve Name stealer \r character creates long space that is detectable. \e on the other hand is fully invisible

--- src/hooks/GetFriendPersonaName.cpp
+++ src/hooks/GetFriendPersonaName.cpp
@@ -128,7 +128,7 @@ const char *GetNamestealName(CSteamID steam_id)
             {
 
                 // Return the name that has changed from the func above
-                return format(stolen_name, "\015").c_str();
+                return format(stolen_name, "\e").c_str();
             }
         }
     }
