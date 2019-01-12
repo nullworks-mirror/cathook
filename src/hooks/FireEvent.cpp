57698d3c26c3fd272c3d02353d5e707cb619889d
LightCat <BenCat07@gmail.com>
2019-01-12 17:40:16 +0100

    fix Killstreak

--- src/hooks/FireEvent.cpp
+++ src/hooks/FireEvent.cpp
@@ -5,11 +5,16 @@
 
 #include "HookedMethods.hpp"
 
+namespace hacks::tf2::killstreak
+{
+extern void fire_event(IGameEvent *event);
+}
 namespace hooked_methods
 {
 
 DEFINE_HOOKED_METHOD(FireEvent, bool, IGameEventManager2 *this_, IGameEvent *event, bool no_broadcast)
 {
+    hacks::tf2::killstreak::fire_event(event);
     return original::FireEvent(this_, event, no_broadcast);
 }
 } // namespace hooked_methods
