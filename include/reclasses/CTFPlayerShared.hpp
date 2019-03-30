20e87c02e45753a5c5891611b37a061f3264908e
LightCat <BenCat07@gmail.com>
2019-03-30 12:36:45 +0100

    Add Dominatemark

--- include/reclasses/CTFPlayerShared.hpp
+++ include/reclasses/CTFPlayerShared.hpp
@@ -6,6 +6,13 @@ namespace re
 class CTFPlayerShared
 {
 public:
+    inline static bool IsDominatingPlayer(CTFPlayerShared *self, int ent_idx)
+    {
+        static auto signature = gSignatures.GetClientSignature("55 89 E5 56 53 83 EC ? 8B 5D ? E8 ? ? ? ? 89 C6 31 C0");
+        typedef bool (*IsDominatingPlayer_t)(CTFPlayerShared *, int);
+        static IsDominatingPlayer_t IsDominatingPlayer_fn = IsDominatingPlayer_t(signature);
+        return IsDominatingPlayer_fn(self, ent_idx);
+    }
     inline static float GetCritMult(CTFPlayerShared *self)
     {
         return ((fminf(fmaxf(*(float *) (unsigned(self) + 672) * 0.0039215689f, 0.0f), 1.0f) * 3.0f) + 1.0f);
