09e7c1b77f79dcf256e2cbda93491173678dff09
BenCat07 <BenCat07@gmail.com>
2019-06-28 23:53:29 +0200

    Improve Killstreaks (See Description)
    * Fix KIlls being counted twice ( FireEvent & FIreEventClientside both send event)
    * Fix Killstreak not being applied Correctly
    * Fix Killstreak not showing up on scoreboard
    * Fix Total KIllstreak being applied to weapon instead of weapon specific
    Also fixes #778

--- src/hooks/FireEvent.cpp
+++ src/hooks/FireEvent.cpp
@@ -12,9 +12,11 @@ extern void fire_event(IGameEvent *event);
 namespace hooked_methods
 {
 
+// Unused right now
+// TODO: maybe remove?
 DEFINE_HOOKED_METHOD(FireEvent, bool, IGameEventManager2 *this_, IGameEvent *event, bool no_broadcast)
 {
-    hacks::tf2::killstreak::fire_event(event);
+    // hacks::tf2::killstreak::fire_event(event);
     return original::FireEvent(this_, event, no_broadcast);
 }
 } // namespace hooked_methods
