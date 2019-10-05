7b2b32d97866ea20cbeaf02b3112e35703dd21c2
Unnamed <user@localhost.localdomain>
2019-10-05 02:35:10 +0000

    ipc.cpp: fix possible crash

--- src/ipc.cpp
+++ src/ipc.cpp
@@ -183,16 +183,17 @@ void UpdateTemporaryData()
 
     if (data.connected)
     {
-        IClientEntity *player = g_IEntityList->GetClientEntity(g_IEngine->GetLocalPlayer());
-        if (player)
+        if (CE_GOOD(LOCAL_E))
         {
+            IClientEntity *player = RAW_ENT(LOCAL_E);
+            int m_IDX = LOCAL_E->m_IDX;
             data.ingame.good = true;
             // TODO kills, deaths, shots, hits, headshots
 
             int score_saved = data.ingame.score;
 
-            data.ingame.score      = g_pPlayerResource->GetScore(g_IEngine->GetLocalPlayer());
-            data.ingame.team       = g_pPlayerResource->GetTeam(g_IEngine->GetLocalPlayer());
+            data.ingame.score      = g_pPlayerResource->GetScore(m_IDX);
+            data.ingame.team       = g_pPlayerResource->GetTeam(m_IDX);
             data.ingame.role       = g_pPlayerResource->GetClass(LOCAL_E);
             data.ingame.life_state = NET_BYTE(player, netvar.iLifeState);
             data.ingame.health     = NET_INT(player, netvar.iHealth);
@@ -231,6 +232,9 @@ void UpdateTemporaryData()
 
 void StoreClientData()
 {
+    if (!peer)
+        return;
+
     UpdateServerAddress();
     user_data_s &data = peer->memory->peer_user_data[peer->client_id];
     data.friendid     = g_ISteamUser->GetSteamID().GetAccountID();
