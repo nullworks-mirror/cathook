fc19245f9d954fb388d99b68936bb454da4c69a4
G-4-M-3-R <69512711+G-4-M-3-R@users.noreply.github.com>
2020-09-08 15:59:28 +0930

    Seperate world and skybox nightmode (#1088)
    * Update FrameStageNotify.cpp
    
    * Update world.xml
    
    * Update world.xml
    
    * Update world.xml
    
    * Add files via upload
    
    * Add files via upload

--- src/hooks/visual/FrameStageNotify.cpp
+++ src/hooks/visual/FrameStageNotify.cpp
@@ -13,16 +13,19 @@
 
 static settings::Float nightmode_gui{ "visual.night-mode.gui", "0" };
 static settings::Float nightmode_world{ "visual.night-mode.world", "0" };
+static settings::Float nightmode_skybox{ "visual.night-mode.skybox", "0" };
 static settings::Rgba nightmode_gui_color{ "visual.night-mode.gui-color", "000000FF" };
 static settings::Rgba nightmode_world_color{ "visual.night-mode.world-color", "000000FF" };
+static settings::Rgba nightmode_skybox_color{ "visual.night-mode.skybox-color", "000000FF" };
 static settings::Boolean no_shake{ "visual.no-shake", "true" };
 
 // Should we update nightmode?
 static bool update_nightmode = false;
 
 // Which strings trigger this nightmode option
-std::vector<std::string> world_strings = { "SkyBox", "World" };
-std::vector<std::string> gui_strings   = { "Other", "VGUI" };
+std::vector<std::string> world_strings  = { "World" };
+std::vector<std::string> skybox_strings = { "SkyBox" };
+std::vector<std::string> gui_strings    = { "Other", "VGUI" };
 
 namespace hooked_methods
 {
@@ -51,7 +54,7 @@ DEFINE_HOOKED_METHOD(FrameStageNotify, void, void *this_, ClientFrameStage_t sta
             if (!pMaterial)
                 continue;
 
-            // 0 = do not filter, 1 = Gui filter, 2 = World filter
+            // 0 = do not filter, 1 = Gui filter, 2 = World filter, 3 = Skybox filter
             int should_filter = 0;
             auto name         = std::string(pMaterial->GetTextureGroupName());
 
@@ -62,6 +65,10 @@ DEFINE_HOOKED_METHOD(FrameStageNotify, void, void *this_, ClientFrameStage_t sta
             for (auto &entry : world_strings)
                 if (name.find(entry) != name.npos)
                     should_filter = 2;
+
+            for (auto &entry : skybox_strings)
+                if (name.find(entry) != name.npos)
+                    should_filter = 3;
 
             if (should_filter)
             {
@@ -93,6 +100,20 @@ DEFINE_HOOKED_METHOD(FrameStageNotify, void, void *this_, ClientFrameStage_t sta
                         pMaterial->ColorModulate(draw_color.r, draw_color.g, draw_color.b);
                     if (a != draw_color.a)
                         pMaterial->AlphaModulate((*nightmode_world_color).a);
+                }
+                else if (should_filter == 3 && *nightmode_skybox > 0.0f)
+                {
+                    // Map to PI/2 so we get full color scale
+                    rgba_t draw_color = colors::Fade(colors::white, *nightmode_skybox_color, (*nightmode_skybox / 100.0f) * (PI / 2), 1.0f);
+
+                    // Check for change
+                    float r, g, b, a;
+                    pMaterial->GetColorModulation(&r, &g, &b);
+                    a = pMaterial->GetAlphaModulation();
+                    if (r != draw_color.r || g != draw_color.g || b != draw_color.b)
+                        pMaterial->ColorModulate(draw_color.r, draw_color.g, draw_color.b);
+                    if (a != draw_color.a)
+                        pMaterial->AlphaModulate((*nightmode_skybox_color).a);
                 }
                 else
                 {
@@ -143,7 +164,9 @@ template <typename T> void rvarCallback(settings::VariableBase<T> &, T)
 static InitRoutine init_fsn([]() {
     nightmode_gui.installChangeCallback(rvarCallback<float>);
     nightmode_world.installChangeCallback(rvarCallback<float>);
+    nightmode_skybox.installChangeCallback(rvarCallback<float>);
     nightmode_gui_color.installChangeCallback(rvarCallback<rgba_t>);
     nightmode_world_color.installChangeCallback(rvarCallback<rgba_t>);
+    nightmode_skybox_color.installChangeCallback(rvarCallback<rgba_t>);
 });
 } // namespace hooked_methods
