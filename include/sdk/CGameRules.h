759e1a4aae143d9708806293319a5beeabaffd55
BenCat07 <BenCat07@gmail.com>
2021-05-07 20:06:26 +0200

    Add Instant revive and automatic F4 for MVM

--- include/sdk/CGameRules.h
+++ include/sdk/CGameRules.h
@@ -9,10 +9,10 @@
 class CGameRules
 {
 public:
-    int pad0[12];     // 0    | 48 bytes  | 48
-    int roundmode;    // 48   | 4 bytes   | 52
-    int pad1[1];      // 52   | 4 bytes   | 56
-    int winning_team; // 56   | 4 bytes   | 60
-    char pad2[974];   // 60   | 974 bytes | 1034
-    bool isPVEMode;   // 1034 | 1 byte    | 1035
+    int pad0[12];               // 0    | 48 bytes  | 48
+    int roundmode;              // 48   | 4 bytes   | 52
+    int m_bInWaitingForPlayers; // 52   | 4 bytes   | 56
+    int winning_team;           // 56   | 4 bytes   | 60
+    char pad2[974];             // 60   | 974 bytes | 1034
+    bool isPVEMode;             // 1034 | 1 byte    | 1035
 };
