79eca67a904915d4fecd1a71670174818ab9af71
braaaap master <73117766+d3fc0n6@users.noreply.github.com>
2021-08-19 21:29:43 -0500

    Quit tf2 without causing a crash
    if we do it like this no more crash logs from autojoin

--- src/hacks/AutoJoin.cpp
+++ src/hacks/AutoJoin.cpp
@@ -105,7 +105,7 @@ void updateSearch()
 #if not ENABLE_VISUALS
     if (queue_time.test_and_set(1200000))
     {
-        *(int *) nullptr = 0;
+        g_IEngine->ClientCmd_Unrestricted("quit"); //lol
     }
 #endif
 }
